30f873cee45a51fdab076299a043e82d1c2da915
datnt <dat.nt271102@gmail.com>
2024-07-24 18:40:00 +0700

    dentist register

--- Clinic.Core/Models/DentistDTO.cs
+++ Clinic.Core/Models/DentistDTO.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +14,24 @@ namespace Clinic.Core.Models
         public int YearOfExperience { get; set; }
         public String ClinicDentalId { get; set; }
         public String UserId { get; set; }
+    }
+
+    public class DentistSignUp
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool IsDentist { get; set; } = true;
+        public String? DOB { get; set; }
+        public String? PhoneNumber { get; set; }
+        public bool Sex { get; set; }
+        public int Status { get; set; } = 1;
+        public String Description { get; set; }
+        public String LicenseNumber { get; set; }
+        public int YearOfExperience { get; set; }
+        public String ClinicDentalId { get; set; }
     }
 
     public class UpdateDentist
